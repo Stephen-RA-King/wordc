c083bdc1440464aad9d5f33d69a4865b7b70a7cd
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2024-05-09 20:04:15 +0100

    test: add tests

--- tests/test_integration_01_nominal.py
+++ tests/test_integration_01_nominal.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parents[0]
 
-expected = """4284 the
+expected01 = """4284 the
 2192 and
 2185 of
 1861 a
@@ -26,11 +26,26 @@ expected = """4284 the
 458 at
 """
 
+expected02 = """4284 the
+2192 and
+2185 of
+1861 a
+1685 to
+"""
 
-def test_command_line_usage():
+
+def test_nominal():
     test_file = BASE_DIR / "resources" / "mobydick.txt"
     result = subprocess.run(["wordc", test_file], capture_output=True, text=True)
     assert result.returncode == 0
     assert result.stderr == ""
-    assert result.stdout == expected
+    assert result.stdout == expected01
+
+
+def test_argument_t():
+    test_file = BASE_DIR / "resources" / "mobydick.txt"
+    result = subprocess.run(["wordc", test_file, "-t", "5"], capture_output=True, text=True)
+    assert result.returncode == 0
+    assert result.stderr == ""
+    assert result.stdout == expected02
 
--- tests/test_main_read_file_chunks.py
+++ tests/test_main_read_file_chunks.py
@@ -1,5 +1,4 @@
-
-
+import pytest
 from wordc import wordc
 from pathlib import Path
 
@@ -10,7 +9,26 @@ BASE_DIR = Path(__file__).parents[0]
 def test_nominal():
     test_file = BASE_DIR / "resources" / "small.txt"
     gen = wordc.read_file_chunks(test_file, "utf-8", 1024)
-    for text in gen:
-        assert text == "hello "
+    expected_words = ["hello ", "world\n"]
 
+    for expected_word in expected_words:
+        generated_word = next(gen)
+        assert generated_word == expected_word
+
+    with pytest.raises(StopIteration):
+        next(gen)
+
+
+def test_minimal_chunks():
+    test_file = BASE_DIR / "resources" / "100_bytes.txt"
+    gen = wordc.read_file_chunks(test_file, "utf-8", 10)
+    expected_words = ["Now, in ", "his heart, ", "Ahab had ", "some\nglimpse of ",
+                      "this\n"]
+
+    for expected_word in expected_words:
+        generated_word = next(gen)
+        assert generated_word == expected_word
+
+    with pytest.raises(StopIteration):
+        next(gen)
 
